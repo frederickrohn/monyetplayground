c615781f9d5ae1b14f91cc33eac5f55d325e73e4
frederickrohn <fnr2009@nyu.edu>
2023-09-26 21:59:29 -0400

    add a fixed header

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ var i = 0;
 var txt = 'hi, its fred.'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
+
+document.addEventListener("load", typeWriter());
+window.addEventListener("scroll", () => {
+  console.log("activated!");
+  var header = document.getElementById("menu");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
 function typeWriter() {
+  console.log("typewriter is going!");
   if (i < txt.length) {
     document.getElementById("title-text").innerHTML += txt.charAt(i);
     i++;
@@ -10,4 +20,8 @@ function typeWriter() {
   }
 }
 
-document.onload(typeWriter());
+function headerSwitch(){
+  console.log("activated!");
+  var header = document.getElementById("menu");
+  header.classList.toggle("sticky", window.scrollY > 0);
+};
